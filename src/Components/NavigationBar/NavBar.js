da0c80da1011f7e1d0fa4a00ba6a98e62ae91e68
Owais Khan <ovi240696@gmail.com>
2020-05-28 22:01:44 +0500

    changes done

--- src/Components/NavigationBar/NavBar.js
+++ src/Components/NavigationBar/NavBar.js
@@ -2,20 +2,23 @@ import React from "react";
 // import "./App.css";
 import { Link } from "react-router-dom";
 function NavBar() {
+	const NAVSTYLE = {
+		color: "white",
+	};
 	return (
 		<nav>
 			<ul className="nav-links">
-				<Link to="/">
+				<Link style={NAVSTYLE} to="/">
 					<li> Home</li>
 				</Link>
-				<Link to="/about">
+				<Link style={NAVSTYLE} to="/about">
 					<li> About</li>
 				</Link>
-				<Link to="/contact">
-					<li> Contact</li>
-				</Link>
-				<Link to="/gallery">
+				<Link style={NAVSTYLE} to="/gallery">
 					<li> Gallery</li>
+				</Link>
+				<Link style={NAVSTYLE} to="/contact">
+					<li> Contact</li>
 				</Link>
 			</ul>
 		</nav>
--- src/Components/Pages/Home/home.js
+++ src/Components/Pages/Home/home.js
@@ -2,9 +2,112 @@ import React from "react";
 
 function HomeContent() {
 	return (
-		<div>
-			<h1> Hello Home</h1>
-		</div>
+		<section id="services" className="features-area">
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-lg-6 col-md-10">
+						<div className="section-title text-center pb-10">
+							<h3 className="title">Welcome to Home Page</h3>
+							<p className="text">
+								Stop wasting time and money designing and
+								managing a website that doesn’t get results.
+								Happiness guaranteed!
+							</p>
+						</div>{" "}
+						{/* row */}
+					</div>
+				</div>{" "}
+				{/* row */}
+				<div className="row justify-content-center">
+					<div className="col-lg-4 col-md-7 col-sm-9">
+						<div className="single-features mt-40">
+							<div className="features-title-icon d-flex justify-content-between">
+								<h4 className="features-title">
+									<a href="#">Graphics Design</a>
+								</h4>
+								<div className="features-icon">
+									<i className="lni lni-brush" />
+									<img
+										className="shape"
+										src="assets/images/f-shape-1.svg"
+										alt="Shape"
+									/>
+								</div>
+							</div>
+							<div className="features-content">
+								<p className="text">
+									Short description for the ones who look for
+									something new. Short description for the
+									ones who look for something new.
+								</p>
+								<a className="features-btn" href="#">
+									LEARN MORE
+								</a>
+							</div>
+						</div>{" "}
+						{/* single features */}
+					</div>
+					<div className="col-lg-4 col-md-7 col-sm-9">
+						<div className="single-features mt-40">
+							<div className="features-title-icon d-flex justify-content-between">
+								<h4 className="features-title">
+									<a href="#">Website Design</a>
+								</h4>
+								<div className="features-icon">
+									<i className="lni lni-layout" />
+									<img
+										className="shape"
+										src="assets/images/f-shape-1.svg"
+										alt="Shape"
+									/>
+								</div>
+							</div>
+							<div className="features-content">
+								<p className="text">
+									Short description for the ones who look for
+									something new. Short description for the
+									ones who look for something new.
+								</p>
+								<a className="features-btn" href="#">
+									LEARN MORE
+								</a>
+							</div>
+						</div>{" "}
+						{/* single features */}
+					</div>
+					<div className="col-lg-4 col-md-7 col-sm-9">
+						<div className="single-features mt-40">
+							<div className="features-title-icon d-flex justify-content-between">
+								<h4 className="features-title">
+									<a href="#">Digital Marketing</a>
+								</h4>
+								<div className="features-icon">
+									<i className="lni lni-bolt" />
+									<img
+										className="shape"
+										src="assets/images/f-shape-1.svg"
+										alt="Shape"
+									/>
+								</div>
+							</div>
+							<div className="features-content">
+								<p className="text">
+									Short description for the ones who look for
+									something new. Short description for the
+									ones who look for something new.
+								</p>
+								<a className="features-btn" href="#">
+									LEARN MORE
+								</a>
+							</div>
+						</div>{" "}
+						{/* single features */}
+					</div>
+				</div>{" "}
+				{/* row */}
+			</div>{" "}
+			{/* container */}
+		</section>
 	);
 }
 
